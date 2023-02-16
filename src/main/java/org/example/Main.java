290ab7a1cb6d5149ed4b6aa233c6d27e15ff37fa
Abdelkarim555 <abdelkarim.sallami@hotmail.com>
2023-02-16 12:38:17 +0100

    test1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Geef hostname: ");
+        String hostname = sc.nextLine();
+
+        System.out.println("Geef port: ");
+        String port = sc.nextLine();
+
+        try
+        {
+
+            Runtime.getRuntime().exec(new String[] {"cmd", "/K", "ssh -p "+port+" root@"+hostname}
+                    Runtime.getRuntime().exec(new String[] {"cmd", "/K", "ssh -p "+port+" root@"+hostname});
+        }
+        catch (Exception e)
+        {
+            System.out.println("Iets fout gegaan");
+            e.printStackTrace();
+        }
+
         System.out.println("gwn test");
     }
 }
