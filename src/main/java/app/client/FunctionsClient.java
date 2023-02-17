bd4e99964fc53e0e8c479375540815c3f1c304fd
joshu <joshua.eijbersen@htomail.com>
2023-02-17 22:07:25 +0100

    Client functions toegevoegd, nog niet getest.

---
+++ src/main/java/app/client/FunctionsClient.java
@@ -0,0 +1,91 @@
+package app.client;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Functions {
+    private static Socket socket;
+    private static String hostName;
+    private static int portNumber;
+
+    public static void connectToServer()
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Geef hostname: ");
+        String hostname = sc.nextLine();
+
+        System.out.println("Geef port: ");
+        String port = sc.nextLine();
+        new Thread()
+        {
+            @Override
+            public void run()
+            {
+                try
+                {
+                    hostName = hostname;
+                    portNumber = Integer.parseInt(port);
+                    socket = new Socket(hostname,Integer.parseInt(port));
+                }
+                catch (IOException ex)
+                {
+                    Logger.getLogger(Functions.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }.start();
+    }
+
+    private static void reconnectToServer()
+    {
+        try
+        {
+            socket = new Socket(hostName, portNumber);
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(Functions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void receiveFile(String outputFilePath)
+    {
+        InputStream is;
+        BufferedInputStream bis;
+        FileOutputStream fos;
+        BufferedOutputStream bos;
+        try
+        {
+            File output = new File(outputFilePath);
+            is = socket.getInputStream();
+            bis = new BufferedInputStream(is);
+            fos = new FileOutputStream(output);
+            bos = new BufferedOutputStream(fos);
+            byte[] buffer = new byte[1024];
+            int data;
+            while(true)
+            {
+                data = bis.read(buffer);
+                if(data != -1)
+                {
+                    bos.write(buffer, 0, 1024);
+                }
+                else
+                {
+                    bis.close();
+                    bos.close();
+                    break;
+                }
+            }
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(Functions.class.getName()).log(Level.SEVERE, null, ex);
+        }
+//        reconnectToServer();
+    }
+}
--- src/main/java/app/client/Main.java
+++ src/main/java/app/client/Main.java
@@ -1,4 +1,15 @@
-package org.example;
+//package org.example;
+
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import java.util.Scanner;
 
@@ -15,8 +26,9 @@ public class Main {
         try
         {
 
-            Runtime.getRuntime().exec(new String[] {"cmd", "/K", "ssh -p "+port+" root@"+hostname}
+            Runtime.getRuntime().exec(new String[] {"cmd", "/K", "ssh -p "+port+" root@"+hostname});
                     Runtime.getRuntime().exec(new String[] {"cmd", "/K", "ssh -p "+port+" root@"+hostname});
+            System.out.println("Het werkt.");
         }
         catch (Exception e)
         {
