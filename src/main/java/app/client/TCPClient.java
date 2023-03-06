15cb72c313c046fb77f612c70f24cbc7a15248c6
joshu <joshua.eijbersen@htomail.com>
2023-03-06 10:24:09 +0100

    UDP communication

--- src/main/java/app/client/TCPClient.java
+++ src/main/java/app/client/TCPClient.java
@@ -5,26 +5,42 @@ import java.net.*;
 
 public class TCPClient {
     public static void main(String[] args) throws Exception {
-        Socket socket = new Socket("localHost", 2011);
+//        Socket socket = new Socket("localHost", 1234);
+//
+//        FileOutputStream fos = new FileOutputStream("C:\\Users\\joshu\\Documents\\Level1Test.txt");
+//        BufferedOutputStream bos = new BufferedOutputStream(fos);
+//
+//        InputStream is = socket.getInputStream();
+//        byte[] contents = new byte[10000];
+//
+//        int bytesRead;
+//        while((bytesRead = is.read(contents)) != -1) {
+//            bos.write(contents, 0, bytesRead);
+//        }
+//
+//        bos.flush();
+//        fos.flush();
+//        System.out.println("File saved successfully!");
+//
+//        bos.close();
+//        fos.close();
+//        is.close();
+//        socket.close();
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress serverAddress = InetAddress.getByName("localhost");
 
-        FileOutputStream fos = new FileOutputStream("C:\\Users\\joshu\\Documents\\Level1Test.txt");
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        byte[] sendData = "send file".getBytes();
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 1234);
+        clientSocket.send(sendPacket);
 
-        InputStream is = socket.getInputStream();
-        byte[] contents = new byte[10000];
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String fileContents = new String(receivePacket.getData());
 
-        int bytesRead;
-        while((bytesRead = is.read(contents)) != -1) {
-            bos.write(contents, 0, bytesRead);
-        }
+        System.out.println("Received file contents:");
+        System.out.println(fileContents);
 
-        bos.flush();
-        fos.flush();
-        System.out.println("File saved successfully!");
-
-        bos.close();
-        fos.close();
-        is.close();
-        socket.close();
+        clientSocket.close();
     }
 }
--- src/main/java/app/server/TCPServer.java
+++ src/main/java/app/server/TCPServer.java
@@ -5,59 +5,87 @@ import java.net.*;
 
 public class TCPServer {
     public static void main(String[] args) throws Exception {
-        ServerSocket serverSocket = new ServerSocket(2011);
-        serverSocket.setReuseAddress(true);
-        System.out.println("Server listening on port 2011...");
+//        ServerSocket serverSocket = new ServerSocket(1234);
+//        serverSocket.setReuseAddress(true);
+//        System.out.println("Server listening on port 1234...");
+        DatagramSocket serverSocket = new DatagramSocket(1234);
+        System.out.println("Server listening on port 1234...");
 
         while (true) {
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
-            Thread t = new Thread(new ClientHandler(clientSocket));
-            t.start();
+//            Socket clientSocket = serverSocket.accept();
+//            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+//            Thread t = new Thread(new ClientHandler(clientSocket));
+//            t.start();
+
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            serverSocket.receive(receivePacket);
+            System.out.println("Received packet from client: " + receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort());
+
+            String fileContents = readFileContents("C:\\Users\\joshu\\Documents\\TestTCP1.txt");
+            byte[] sendData = fileContents.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), receivePacket.getPort());
+            serverSocket.send(sendPacket);
+
+            System.out.println("File sent to client: " + receivePacket.getAddress().getHostAddress() + ":" + receivePacket.getPort());
         }
     }
 
-    static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
+    private static String readFileContents(String filePath) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String line;
 
-        public ClientHandler(Socket socket) {
-            this.clientSocket = socket;
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+            sb.append(System.lineSeparator());
         }
 
-        @Override
-        public void run() {
-            try {
-
-                File file = new File("C:\\Users\\joshu\\Documents\\Level1.txt");
-                FileInputStream fis = new FileInputStream(file);
-                BufferedInputStream bis = new BufferedInputStream(fis);
-
-                OutputStream os = clientSocket.getOutputStream();
-                byte[] contents;
-                long fileLength = file.length();
-                long current = 0;
-
-                while(current!=fileLength) {
-                    int size = 10000;
-                    if(fileLength - current >= size)
-                        current += size;
-                    else {
-                        size = (int)(fileLength - current);
-                        current = fileLength;
-                    }
-                    contents = new byte[size];
-                    bis.read(contents, 0, size);
-                    os.write(contents);
-                    System.out.println("Sending file ... " + (current*100)/fileLength + "% complete!");
-                }
-        os.flush();
-        bis.close();
-        fis.close();
-        clientSocket.close();
-        System.out.println("File sent to client: " + clientSocket.getInetAddress().getHostAddress());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        br.close();
+        return sb.toString();
     }
-}
+
+//    static class ClientHandler implements Runnable {
+//        private final Socket clientSocket;
+//
+//        public ClientHandler(Socket socket) {
+//            this.clientSocket = socket;
+//        }
+//
+//        @Override
+//        public void run() {
+//            try {
+//
+//                File file = new File("C:\\Users\\joshu\\Documents\\Level1.txt");
+//                FileInputStream fis = new FileInputStream(file);
+//                BufferedInputStream bis = new BufferedInputStream(fis);
+//
+//                OutputStream os = clientSocket.getOutputStream();
+//                byte[] contents;
+//                long fileLength = file.length();
+//                long current = 0;
+//
+//                while(current!=fileLength) {
+//                    int size = 10000;
+//                    if(fileLength - current >= size)
+//                        current += size;
+//                    else {
+//                        size = (int)(fileLength - current);
+//                        current = fileLength;
+//                    }
+//                    contents = new byte[size];
+//                    bis.read(contents, 0, size);
+//                    os.write(contents);
+//                    System.out.println("Sending file ... " + (current*100)/fileLength + "% complete!");
+//                }
+//        os.flush();
+//        bis.close();
+//        fis.close();
+//        clientSocket.close();
+//        System.out.println("File sent to client: " + clientSocket.getInetAddress().getHostAddress());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//}
 }
